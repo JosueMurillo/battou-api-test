10332f104391069289c1120e8d8d9441f540bc76
Josue Murillo <josue.murillo@avantica.net>
2018-09-26 15:31:50 -0600

    added code

--- routes/orders.js
+++ routes/orders.js
@@ -5,10 +5,6 @@ var config = require("../config/config.json");
 const axios = require('axios'); 
 const { check, validationResult } = require('express-validator/check');
 
-//Ids of the metafields that store the secuential number.
-const generalId= "4308630863935";
-const clubId= "4308639121471";
-
 /**
  * Updates an order and adds provided metadata
  * Params:
@@ -37,10 +33,10 @@ router.put('/metadata', [
         return res.status(400).json({ errors: errors.array() });
     }
     
-    var orderId = req.body.orderId
+    var orderId = req.body.orderId;
     var data = JSON.parse(req.body.metadata);
-    var generalCurrentConsecutive =0;
-    var clubCurrentConsecutive =0;
+    var generalCurrentConsecutive = 0;
+    var clubCurrentConsecutive = 0;
     var shopifyOrderOb = {
         order: {
             id: orderId,
@@ -48,101 +44,81 @@ router.put('/metadata', [
         }
     };
 
-   
+    var promises = [];
 
-    
+    promises.push(api.getData('/admin/metafields/'+config.store.generalId+'.json'));
+    promises.push(api.getData('/admin/metafields/'+config.store.clubId+'.json'));
 
- //Gets the metafield of ticket Number
-    api.getData('/admin/metafields/'+generalId+'.json')
-      .then(response => {
-        var metafielddata= response["metafield"];
-        //Get the last ticket number for General tickets stored on the metafield.
-        Object.keys(metafielddata).forEach(function(key) {
-          if (key=='value') {
-            generalCurrentConsecutive = metafielddata[key];
-          }
-        });
-
-        //Get the last ticket number for Club tickets stored on the metafield.
-        api.getData('/admin/metafields/'+clubId+'.json')
-          .then(response => {
-            var metafielddata= response["metafield"];
-            //Get the last ticket number stored on the metafield.
-            Object.keys(metafielddata).forEach(function(key) {
-              if (key=='value') {
-                clubCurrentConsecutive = metafielddata[key];
-              }
-            });
-            Object.keys(data).forEach(function(key) {
-            var value = data[key];
+    Promise.all(promises).then(results => {
+      //Get the last ticket number for General tickets stored on the metafield.
+      generalCurrentConsecutive = results[0].metafield.value;
+      clubCurrentConsecutive = results[1].metafield.value;
         
-           if(key === 'ticket_list') {
-            // Updates the number for every ticket and increases the consecutive.
-            Object.keys(data['ticket_list']).forEach(function(ticketkey) {
-              if (data['ticket_list'][ticketkey].ticket_type == 'general') {
-                data['ticket_list'][ticketkey].number = generalCurrentConsecutive+1;
-                generalCurrentConsecutive++;
-              }
-              else{
-                data['ticket_list'][ticketkey].number = clubCurrentConsecutive+1;
-                clubCurrentConsecutive++;
-              }
-            
-            });
+      Object.keys(data).forEach(function(key) {
+        var value = data[key];
+    
+        if(key === 'ticket_list') {
+          // Updates the number for every ticket and increases the consecutive.
+          Object.keys(data['ticket_list']).forEach(function(ticketkey) {
+            if (data['ticket_list'][ticketkey].ticket_type == 'general') {
+              data['ticket_list'][ticketkey].number = generalCurrentConsecutive+1;
+              generalCurrentConsecutive++;
+            }
+            else{
+              data['ticket_list'][ticketkey].number = clubCurrentConsecutive+1;
+              clubCurrentConsecutive++;
+            }
+          
+          });
 
           value = JSON.stringify(data[key]);
-          }
-          shopifyOrderOb.order.metafields.push({
-              key: key,
-              value: value,
-              value_type: "string",
-              namespace: "global"
-          });
+        }
 
-          });
-          //Puts the data on the orders metafield 
-          api.putData('/admin/orders/'+orderId+'.json', shopifyOrderOb)
-            .then(response => {
-              var shopifyGeneralConsecutiveMetafieldOb = {
-                metafield: {
-                    value: generalCurrentConsecutive
-                }
-              };
-              //Changes the Generalnumber consecutive metafield to the last number given to a ticket.
-              api.putData('/admin/metafields/'+generalId+'.json',shopifyGeneralConsecutiveMetafieldOb)
-              .then(response => {
-                //res.send(response);
-              })
-              .catch(error => {
-                res.status(400).send(error.response.data);
-              });
-              //Changes the Club number consecutive metafields to the last number given to a club ticket
-              var shopifyClubConsecutiveMetafieldOb = {
-                metafield: {
-                    value: clubCurrentConsecutive
-                }
-              };
-              api.putData('/admin/metafields/'+clubId+'.json',shopifyClubConsecutiveMetafieldOb)
-              .then(response => {
-                //res.send(response);
-              })
-              .catch(error => {
-                res.status(400).send(error.response.data);
-              });
-              res.send(response);
-            })
-            .catch(error => {
-              res.status(400).send(error.response.data);
-            });
+        shopifyOrderOb.order.metafields.push({
+            key: key,
+            value: value,
+            value_type: "string",
+            namespace: "global"
+        });
 
-      })
-      .catch(error => {
+      }); //foreach data keys
+
+      //Puts the data on the orders metafield 
+      api.putData('/admin/orders/'+orderId+'.json', shopifyOrderOb)
+        .then(response => {
+          var shopifyGeneralConsecutiveMetafieldOb = {
+            metafield: {
+                value: generalCurrentConsecutive
+            }
+          };
+          //Changes the Generalnumber consecutive metafield to the last number given to a ticket.
+          api.putData('/admin/metafields/'+config.store.generalId+'.json',shopifyGeneralConsecutiveMetafieldOb)
+          .then(response => {
+            //res.send(response);
+          })
+          .catch(error => {
+            res.status(400).send(error.response.data);
+          });
+          //Changes the Club number consecutive metafields to the last number given to a club ticket
+          var shopifyClubConsecutiveMetafieldOb = {
+            metafield: {
+                value: clubCurrentConsecutive
+            }
+          };
+          api.putData('/admin/metafields/'+config.store.clubId+'.json',shopifyClubConsecutiveMetafieldOb)
+          .then(response => {
+            //res.send(response);
+          })
+          .catch(error => {
+            res.status(400).send(error.response.data);
+          });
+          
+          res.send(response);
+        }).catch(error => {
         res.status(400).send(error.response.data);
       });
-      })
-    .catch(error => {
-      res.status(400).send(error.response.data);
-    });
+
+    }); //promise
 });
 
 
